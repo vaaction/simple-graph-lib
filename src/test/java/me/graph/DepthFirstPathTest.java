641f05301aae12ec1ff37211ca16878867065bc6
ibukanov <ibukanov87@gmail.com>
2015-07-17 13:11:12 +0300

    graph

--- src/test/java/me/graph/DepthFirstPathTest.java
+++ src/test/java/me/graph/DepthFirstPathTest.java
@@ -1,15 +1,15 @@
 package me.graph;
 
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.Iterator;
-import java.util.List;
 import java.util.stream.IntStream;
 
 public class DepthFirstPathTest {
     public static void main(String[] args) {
         Graph<Integer> directedGraph = new DirectedGraph<>();
         Graph<Integer> undirectedGraph = new UndirectedGraph<>();
-        List<Graph<Integer>> graphs = Arrays.asList(directedGraph, undirectedGraph);
+        Collection<Graph<Integer>> graphs = Arrays.asList(directedGraph, undirectedGraph);
 
         for (Graph<Integer> graph: graphs) {
             IntStream.range(1, 13).forEach(graph::addVertex);
@@ -28,39 +28,26 @@ public class DepthFirstPathTest {
             graph.addEdge(11, 12);
         }
 
-        directedGraph = graphs.get(0);
-        undirectedGraph = graphs.get(1);
-        Iterator<Integer> directedGraphPath = new DepthFirstPath<>(directedGraph, 1, 9).getPath();
-        Iterator<Integer> undirectedGraphPath = new DepthFirstPath<>(undirectedGraph, 1, 9).getPath();
-        System.out.print("\ndirectedGraph path 1: ");
-        directedGraphPath.forEachRemaining(v -> System.out.print(v + " "));
-        System.out.print("\nundirectedGraph path 1: ");
-        undirectedGraphPath.forEachRemaining(v -> System.out.print(v + " "));
+        graphs.forEach(graph -> {
+            Iterator<Integer> graphPath = new DepthFirstPath<>(graph, 1, 9).getPath();
+            System.out.print("\ngraph path for " + graph + ": ");
+            graphPath.forEachRemaining(v -> System.out.print(v + " "));
 
-        directedGraphPath = new DepthFirstPath<>(directedGraph, 3, 8).getPath();
-        undirectedGraphPath = new DepthFirstPath<>(undirectedGraph, 3, 8).getPath();
-        System.out.print("\ndirectedGraph path 2: ");
-        directedGraphPath.forEachRemaining(v -> System.out.print(v + " "));
-        System.out.print("\nundirectedGraph path 2: ");
-        undirectedGraphPath.forEachRemaining(v -> System.out.print(v + " "));
+            graphPath = new DepthFirstPath<>(graph, 3, 8).getPath();
+            System.out.print("\ngraph path for " + graph + ": ");
+            graphPath.forEachRemaining(v -> System.out.print(v + " "));
 
-        directedGraphPath = new DepthFirstPath<>(directedGraph, 1, 10).getPath();
-        System.out.print("\ndirectedGraph path 3 is empty: " + !directedGraphPath.hasNext());
-        undirectedGraphPath = new DepthFirstPath<>(undirectedGraph, 1, 10).getPath();
-        System.out.print("\nundirectedGraph path 3: ");
-        undirectedGraphPath.forEachRemaining(v -> System.out.print(v + " "));
+            graphPath = new DepthFirstPath<>(graph, 1, 10).getPath();
+            System.out.print("\ngraph path for " + graph + ": ");
+            graphPath.forEachRemaining(v -> System.out.print(v + " "));
 
-        directedGraphPath = new DepthFirstPath<>(directedGraph, 5, 11).getPath();
-        System.out.print("\ndirectedGraph path 4 is empty: " + !directedGraphPath.hasNext());
-        undirectedGraphPath = new DepthFirstPath<>(undirectedGraph, 5, 11).getPath();
-        System.out.print("\nundirectedGraph path 4 is empty: " + !undirectedGraphPath.hasNext());
+            graphPath = new DepthFirstPath<>(graph, 5, 11).getPath();
+            System.out.print("\ngraph path for "+ graph + " is empty: " + !graphPath.hasNext());
 
-        directedGraphPath = new DepthFirstPath<>(directedGraph, 11, 12).getPath();
-        undirectedGraphPath = new DepthFirstPath<>(undirectedGraph, 11, 12).getPath();
-        System.out.print("\ndirectedGraph path 5: ");
-        directedGraphPath.forEachRemaining(v -> System.out.print(v + " "));
-        System.out.print("\nundirectedGraph path 5: ");
-        undirectedGraphPath.forEachRemaining(v -> System.out.print(v + " "));
-        System.out.print("\n");
+            graphPath = new DepthFirstPath<>(graph, 11, 12).getPath();
+            System.out.print("\ngraph path for "+ graph + ": ");
+            graphPath.forEachRemaining(v -> System.out.print(v + " "));
+            System.out.print("\n");
+        });
     }
 }
